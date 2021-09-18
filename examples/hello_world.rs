2986a6d3fc9f9dbb0e2537adc8ab0c6e9d35deb9
jm.fiaschi <jm.fiaschi@gmail.com>
2021-09-18 18:48:49 +0200

    doc(project): add hello-world (#8)

---
+++ examples/hello_world.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+#[async_std::main]
+async fn main() -> io::Result<()> {
+    let config = r#"
+    [{
+        "type": "r",
+        "conn": {
+            "type": "mem",
+            "data": "Hello World !!!"
+        },
+        "doc": { "type": "text" }
+    },
+    {
+        "type": "w"
+    }]
+    "#;
+    let config = serde_json::from_str(config.to_string().as_str())?;
+
+    chewdata::exec(config, None).await
+}
