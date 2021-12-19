93105c8288615108ac86abc6b7671bf3dcdce0fa
github-action-benchmark <github@users.noreply.github.com>
2021-12-19 17:32:52 +0000

    add Benchmark (cargo) benchmark result for ba19741348171e01fca4c21c6c4d98b2bb09abea

---
+++ docs/benches/alpha/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1639935172204,
+  "repoUrl": "https://github.com/jmfiaschi/chewdata",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "distinct": true,
+          "id": "ba19741348171e01fca4c21c6c4d98b2bb09abea",
+          "message": "chore(release): commit changelog file",
+          "timestamp": "2021-12-19T17:28:15+01:00",
+          "tree_id": "c747843cfd14ed93ed58e98b32a4d6d7ea6fdd51",
+          "url": "https://github.com/jmfiaschi/chewdata/commit/ba19741348171e01fca4c21c6c4d98b2bb09abea"
+        },
+        "date": 1639935171611,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_json/",
+            "value": 183,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_jsonl/",
+            "value": 162,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_xml/",
+            "value": 159381,
+            "range": "± 369",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_csv/",
+            "value": 24128,
+            "range": "± 28",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_toml/",
+            "value": 292,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_yaml/",
+            "value": 466,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
