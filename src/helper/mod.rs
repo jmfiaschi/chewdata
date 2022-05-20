dc3dcc61fb1f05276bedeeb96b29323d80cef96e
jmfiaschi <jm.fiaschi@gmail.com>
2022-05-20 23:38:19 +0200

    chore(test): improve test speed

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -7,8 +7,9 @@ pub mod mustache;
 
 /// Replace a HashMap of readers by HashMap of Values. Each Value indexed by the referencial name.
 ///
-/// # Example
-/// ```rust
+/// # Examples
+///
+/// ```no_run
 /// use chewdata::step::reader::Reader;
 /// use chewdata::connector::in_memory::InMemory;
 /// use chewdata::connector::{Connector, ConnectorType};
@@ -46,7 +47,7 @@ pub async fn referentials_reader_into_value(
     for (name, ref mut referential) in referentials {
         let (sender, receiver) = crossbeam::channel::unbounded();
         let mut values: Vec<Value> = Vec::new();
-        
+
         referential.set_sender(sender);
         referential.exec().await?;
 
@@ -57,4 +58,32 @@ pub async fn referentials_reader_into_value(
     }
 
     Ok(referentials_vec)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::connector::{ConnectorType, in_memory::InMemory};
+
+    use super::*;
+
+    #[async_std::test]
+    async fn referentials_reader_into_value() {
+        let referential_1 = Reader {
+            connector_type: ConnectorType::InMemory(InMemory::new(r#"[{"column1":"value1"}]"#)),
+            ..Default::default()
+        };
+        let referential_2 = Reader {
+            connector_type: ConnectorType::InMemory(InMemory::new(r#"[{"column1":"value2"}]"#)),
+            ..Default::default()
+        };
+        let mut referentials = HashMap::default();
+        referentials.insert("ref_1".to_string(), referential_1);
+        referentials.insert("ref_2".to_string(), referential_2);
+        let values = super::referentials_reader_into_value(referentials).await.unwrap();
+        let values_expected: HashMap<String, Vec<Value>> = serde_json::from_str(
+            r#"{"ref_1":[{"column1":"value1"}],"ref_2":[{"column1":"value2"}]}"#,
+        )
+        .unwrap();
+        assert_eq!(values_expected, values);
+    }
 }
