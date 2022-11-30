5133b97b145b939593eebfd4f59d9c35fa3024fe
jmfiaschi <jmfiaschi@veepee.com>
2022-11-30 01:27:46 +0100

    feat(examples): add example for psql

--- .config/psql.sql
+++ .config/psql.sql
@@ -84,3 +84,25 @@ CREATE TABLE send_with_key (
 );
 
 INSERT INTO send_with_key VALUES (1, 1561, 'value to test 5416', 'Long val\nto test', FALSE, 'é','field must be renamed','2019-12-31',1000000,10.156,'?search=test me','A,B,C','value_to_map','field to remove',null, '[1,2]', '{"field":"value"}');
+
+CREATE SCHEMA examples;
+
+CREATE TABLE examples.simple_insert (
+    "number" INT PRIMARY KEY,
+    "group" INT NULL,
+    "string" TEXT NULL,
+    "long-string" TEXT NULL,
+    "boolean" BOOL NULL,
+    "special_char" CHAR(10) NULL,
+    "rename_this" TEXT NULL,
+    "date" DATE NULL DEFAULT CURRENT_DATE,
+    "filesize" INT NULL,
+    "round" FLOAT NULL,
+    "url" TEXT NULL,
+    "list_to_sort" TEXT NULL,
+    "code" TEXT NULL,
+    "remove_field" TEXT NULL,
+    "null" TEXT null,
+    "array" jsonb,
+    "object" jsonb
+);
