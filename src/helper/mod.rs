1fda46e89e0fdad2a7f446521ef35f89481f2ed3
Jean-Marc Fiaschi <jm.fiaschi@gmail.com>
2023-08-23 17:12:42 +0200

    fix(xml): add xml2json only if xml feature enable

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -6,6 +6,8 @@ use std::{collections::HashMap, io, time::Duration};
 
 pub mod json_pointer;
 pub mod mustache;
+
+#[cfg(feature = "xml")]
 pub mod xml2json;
 
 /// Replace a HashMap of readers by HashMap of Values. Each Value indexed by the referencial name.
